fc8592ced2781fbb932c5aa50890ace95856e4c7
Vadim <66244188+VadimsKovalenko@users.noreply.github.com>
2024-12-11 19:48:52 +0100

    Remove package res

--- Package.swift
+++ Package.swift
@@ -14,21 +14,11 @@ let package = Package(
             name: "AppStunSDK",
             targets: ["AppStunSDK"]),
     ],
-    dependencies: [
-//        .package(url: "https://github.com/mobillium/MobilliumBuilders.git", from: "1.6.1"),
-//        .package(url: "https://github.com/sindresorhus/Defaults.git", from: "8.2.0"),
-//        .package(name: "Qonversion", url: "https://github.com/qonversion/qonversion-ios-sdk.git", from: "5.6.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppStunSDK" //,
-//            dependencies: [
-//                "Defaults",
-//                "MobilliumBuilders",
-//                "Qonversion"
-//            ]
+            name: "AppStunSDK"
         ),
             
 
