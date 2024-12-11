234835102516cc1d02e91a78e79e1e8c48c452d8
Vadim <66244188+VadimsKovalenko@users.noreply.github.com>
2024-12-11 19:28:03 +0100

    Add app stun

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "AppstunSDK",
+    name: "AppStunSDK",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AppstunSDK",
-            targets: ["AppstunSDK"]),
+            name: "AppStunSDK",
+            targets: ["AppStunSDK"]),
     ],
     dependencies: [
         .package(url: "https://github.com/mobillium/MobilliumBuilders.git", from: "1.6.1"),
@@ -23,12 +23,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppstunSDK",
+            name: "AppStunSDK",
             dependencies: [
                 "Defaults",
                 "MobilliumBuilders",
                 "Qonversion"
             ]
-        )
+        ),
+            
+
     ]
 )
--- Sources/Appstun/Appstun.swift
+++ Sources/Appstun/Appstun.swift
@@ -1,4 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-
+open class Test {
+    static func test() {
+        
+    }
+}
